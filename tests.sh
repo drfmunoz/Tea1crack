f1776312f052db297d5e60aef7926971fbc8e600
freddy@pc-25-69-104-200.cm.vtr.net <>
2006-06-26 22:07:17 -0400

    some new files

---
+++ tests.sh
@@ -0,0 +1,10 @@
+#/bin/sh!
+
+./ts_full_test  -i data/prob1\ set1.txt -o results/prob1_set1.data -maxtstl 31 -maxtsiter 4000 -maxtsmdec 128 -itstl 10 -itsiter 1000 -itsmdec 64 -vartstl 5 -vartsiter 1000 -vartsmdec 64 -mintskep 10 -vartskep 20
+./ts_full_test  -i data/prob1\ set2.txt -o results/prob1_set2.data -maxtstl 31 -maxtsiter 4000 -maxtsmdec 128 -itstl 10 -itsiter 1000 -itsmdec 64 -vartstl 5 -vartsiter 1000 -vartsmdec 64 -mintskep 10 -vartskep 20
+#./ts_full_test  -i data/prob1\ set3.txt -o results/prob1_set3.data -maxtstl 31 -maxtsiter 4000 -maxtsmdec 128 -itstl 10 -itsiter 1000 -itsmdec 64 -vartstl 5 -vartsiter 1000 -vartsmdec 64 -mintskep 10 -vartskep 20
+./ts_full_test  -i data/1000generate.data -o results/1000gen.data -maxtstl 31 -maxtsiter 4000 -maxtsmdec 128 -itstl 10 -itsiter 1000 -itsmdec 64 -vartstl 5 -vartsiter 1000 -vartsmdec 64 -mintskep 10 -vartskep 20
+./ts_full_test  -i data/2000generate.data -o results/2000gen.data -maxtstl 31 -maxtsiter 4000 -maxtsmdec 128 -itstl 10 -itsiter 1000 -itsmdec 64 -vartstl 5 -vartsiter 1000 -vartsmdec 64 -mintskep 10 -vartskep 20
+./ts_full_test  -i data/3000generate.data -o results/3000gen.data -maxtstl 31 -maxtsiter 4000 -maxtsmdec 128 -itstl 10 -itsiter 1000 -itsmdec 64 -vartstl 5 -vartsiter 1000 -vartsmdec 64 -mintskep 10 -vartskep 20
+./ts_full_test  -i data/4000generate.data -o results/4000gen.data -maxtstl 31 -maxtsiter 4000 -maxtsmdec 128 -itstl 10 -itsiter 1000 -itsmdec 64 -vartstl 5 -vartsiter 1000 -vartsmdec 64 -mintskep 10 -vartskep 20
+./ts_full_test  -i data/5000generate.data -o results/5000gen.data -maxtstl 31 -maxtsiter 4000 -maxtsmdec 128 -itstl 10 -itsiter 1000 -itsmdec 64 -vartstl 5 -vartsiter 1000 -vartsmdec 64 -mintskep 10 -vartskep 20
